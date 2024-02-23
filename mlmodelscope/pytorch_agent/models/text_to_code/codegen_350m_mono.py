3f34343a890b551f5d983dc11d4ce6ac5dddee48
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-02-23 17:24:45 +0000

    Fix CodeGen-350M models

--- mlmodelscope/pytorch_agent/models/text_to_code/codegen_350m_mono.py
+++ mlmodelscope/pytorch_agent/models/text_to_code/codegen_350m_mono.py
@@ -6,11 +6,13 @@ class PyTorch_Transformers_CodeGen_350M_Mono(PyTorchAbstractClass):
   def __init__(self):
     # https://huggingface.co/docs/transformers/model_doc/codegen 
     checkpoint = "Salesforce/codegen-350M-mono" 
-    self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    self.tokenizer = AutoTokenizer.from_pretrained(checkpoint, padding_side="left")
     self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
 
+    self.tokenizer.pad_token = self.tokenizer.eos_token 
+
   def preprocess(self, input_texts):
-    return self.tokenizer(input_texts, return_tensors="pt") 
+    return self.tokenizer(input_texts, return_tensors="pt", padding=True) 
   
   def predict(self, model_input): 
     return self.model.generate(**model_input, pad_token_id=self.tokenizer.eos_token_id) 
