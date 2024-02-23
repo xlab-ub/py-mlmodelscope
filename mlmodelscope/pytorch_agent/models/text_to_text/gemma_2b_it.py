a9181c46a5727ec564aa06dc13d2233bf5a3570a
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-02-23 12:32:26 -0500

    Update Gemma-2B models

--- mlmodelscope/pytorch_agent/models/text_to_text/gemma_2b_it.py
+++ mlmodelscope/pytorch_agent/models/text_to_text/gemma_2b_it.py
@@ -12,7 +12,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 class PyTorch_Transformers_Gemma_2B_IT(PyTorchAbstractClass):
   def __init__(self):
     self.tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it") 
-    self.model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto") 
+    self.model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it") 
   
   def preprocess(self, input_texts):
     return self.tokenizer(input_texts, return_tensors="pt", padding=True).input_ids 
