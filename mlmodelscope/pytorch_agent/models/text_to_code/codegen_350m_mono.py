a18750dfe0ba98de2e8bbf54d0d1623da38f8a8a
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-02-22 23:15:14 -0500

    Add CodeGen-350M models in pytorch-agent

---
+++ mlmodelscope/pytorch_agent/models/text_to_code/codegen_350m_mono.py
@@ -0,0 +1,19 @@
+from ..pytorch_abc import PyTorchAbstractClass 
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+class PyTorch_Transformers_CodeGen_350M_Mono(PyTorchAbstractClass):
+  def __init__(self):
+    # https://huggingface.co/docs/transformers/model_doc/codegen 
+    checkpoint = "Salesforce/codegen-350M-mono" 
+    self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
+
+  def preprocess(self, input_texts):
+    return self.tokenizer(input_texts, return_tensors="pt") 
+  
+  def predict(self, model_input): 
+    return self.model.generate(**model_input, pad_token_id=self.tokenizer.eos_token_id) 
+
+  def postprocess(self, model_output):
+    return self.tokenizer.batch_decode(model_output, skip_special_tokens=True)
---
+++ pydldataset/datasets/test_code.py
@@ -0,0 +1,20 @@
+import os
+import pathlib 
+
+class Test_Code:
+  def __init__(self): 
+    self.root = os.path.join(pathlib.Path(__file__).parent.resolve(), 'tmp/test_data') 
+    filename = os.path.join(self.root, 'test_code.txt') 
+    with open(filename, 'r') as f: 
+      # https://stackoverflow.com/questions/39921087/a-openfile-r-a-readline-output-without-n
+      self.data = f.read().splitlines() 
+    self.idx = 0 
+  
+  def __len__(self):
+    return 5
+  
+  def __getitem__(self, idx):
+    return self.data[idx] 
+
+def init():
+  return Test_Code()
