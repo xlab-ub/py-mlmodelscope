579a36134884f33a6db79e22629e2eab0d550d13
Harvey <harveykwong@outlook.com>
2024-02-27 22:08:08 -0500

    Added code for Attention_Target_Detection, changed task from eye_contact_det to attention_det, added config support for eye_contact_det

---
+++ mlmodelscope/pytorch_agent/models/attention_detection/eye_contact_cnn.py
@@ -0,0 +1,49 @@
+from ..pytorch_abc import PyTorchAbstractClass
+
+from glob import glob
+import shutil
+import torch
+from time import  strftime
+import os, sys, time
+sys.path.append(os.path.dirname(__file__))
+from src.eye_contact_cnn.demo import run
+
+
+class Eye_Contact_CNN(PyTorchAbstractClass):
+    model_weight = 'eye_contact_cnn/data/model_weights.pkl'
+    face = None
+    video = None
+    jitter = 0
+    save_vis = True
+    save_text = True
+    display_off = False
+
+    def __init__(self, config=None):
+
+        if config is not None:
+            self.model_weight = config["model_weight"]
+            self.face = config["face"]
+            self.video = config["video"]
+            self.jitter = config["jitter"]
+            self.save_vis = config["save_vis"]
+            self.save_text = config["save_text"]
+
+        # The code provided initializes, processes, and runs in a single block of sphagetti code. Will take work to split up.
+       
+    def preprocess(self, input_data):
+        self.input_video_path = input_data
+        return input_data[0]
+        
+
+    def predict(self, input_data):
+        
+        run(input_data, self.face, self.model_weight, self.jitter, self.save_text, self.display_off, self.save_text)
+        return 0
+    
+    def postprocess(self, model_output):
+        print("\nYour outputs are saved at: pydldataset/datasets/tmp/eye_contact_detection_data/outputs!\n")
+        return [0]
+
+
+    
+  
