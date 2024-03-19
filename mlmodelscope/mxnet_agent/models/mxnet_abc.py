34978bef7f38542a6a4dce66b94ad8df57d44bc8
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-03-19 18:34:50 -0400

    Update abstract classes for model file location

--- mlmodelscope/mxnet_agent/models/mxnet_abc.py
+++ mlmodelscope/mxnet_agent/models/mxnet_abc.py
@@ -123,9 +123,9 @@ class MXNetAbstractClass(ABC):
         if not os.path.isdir(temp_path): 
             os.mkdir(temp_path) 
 
-        source_file_name = inspect.stack()[1].filename.replace('\\', '/').split('/')[-1][:-3] 
-        model_symbol_path = os.path.join(temp_path, source_file_name + '/' + model_symbol_url.split('/')[-1]) 
-        model_params_path = os.path.join(temp_path, source_file_name + '/' + model_params_url.split('/')[-1])
+        model_name = inspect.stack()[1].filename.replace('\\', '/').split('/')[-2] 
+        model_symbol_path = os.path.join(temp_path, model_name + '/' + model_symbol_url.split('/')[-1]) 
+        model_params_path = os.path.join(temp_path, model_name + '/' + model_params_url.split('/')[-1])
         if not os.path.exists(model_symbol_path): 
             os.mkdir('/'.join(model_symbol_path.replace('\\', '/').split('/')[:-1])) 
             print("The model symbol file does not exist")
