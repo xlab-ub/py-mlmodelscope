07daa8a2f2d0e396172dbc0a561d0cc84bb40870
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-05-01 06:58:16 -0400

    Update Gemma models in pytorch-agent

--- mlmodelscope/pytorch_agent/models/default/text_to_text/gemma_7b_it/model.py
+++ mlmodelscope/pytorch_agent/models/default/text_to_text/gemma_7b_it/model.py
@@ -1,23 +1,25 @@
 from ....pytorch_abc import PyTorchAbstractClass 
 
-import os 
-from huggingface_hub import login 
-
 from transformers import AutoTokenizer, AutoModelForCausalLM 
 
 class PyTorch_Transformers_Gemma_7B_IT(PyTorchAbstractClass):
   def __init__(self, config=None):
     self.config = config if config else {}
 
-    huggingface_token = os.environ.get("HUGGINGFACE_TOKEN") or self.config.get('huggingface_token')
-    if huggingface_token is None: 
-      raise ValueError("Huggingface token not found. Please set the environment variable HUGGINGFACE_TOKEN or pass it in the config")
-    login(token=huggingface_token)
+    model_id = "google/gemma-7b-it"
+    try:
+      self.tokenizer = AutoTokenizer.from_pretrained(model_id) 
+      self.model = AutoModelForCausalLM.from_pretrained(model_id)
+    except Exception as e:
+      if model_id in e.__str__():
+        self.huggingface_authenticate()
 
-    self.tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it") 
-    self.model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it") 
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id) 
+        self.model = AutoModelForCausalLM.from_pretrained(model_id) 
+      else:
+        raise e
 
-    self.max_new_tokens = self.config['max_new_tokens'] if 'max_new_tokens' in self.config else 32 
+    self.max_new_tokens = self.config.get('max_new_tokens') 
   
   def preprocess(self, input_texts):
     return self.tokenizer(input_texts, return_tensors="pt", padding=True).input_ids 
