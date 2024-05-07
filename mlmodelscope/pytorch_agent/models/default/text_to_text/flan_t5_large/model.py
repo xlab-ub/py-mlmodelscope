6913dd8748584f56693cb3569857b573a1df2ab8
Harvey Jiang <71397990+harveyj123@users.noreply.github.com>
2024-05-07 13:26:03 -0400

    add model sizes for google flan

---
+++ mlmodelscope/pytorch_agent/models/default/text_to_text/flan_t5_large/model.py
@@ -0,0 +1,20 @@
+from ....pytorch_abc import PyTorchAbstractClass
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+class PyTorch_Transformers_FLAN_T5_Base(PyTorchAbstractClass):
+  def __init__(self, config=None):
+    self.config = config if config else {}
+    self.tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
+    self.model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large")
+
+    self.max_new_tokens = self.config['max_new_tokens'] if 'max_new_tokens' in self.config else 32
+
+  def preprocess(self, input_texts):
+    return self.tokenizer(input_texts, return_tensors="pt", padding=True).input_ids
+
+  def predict(self, model_input):
+    return self.model.generate(model_input, max_new_tokens=self.max_new_tokens)
+
+  def postprocess(self, model_output):
+    return self.tokenizer.batch_decode(model_output, skip_special_tokens=True)
