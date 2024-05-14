32d10e70562901ada299d8f9e41daea5c38bdf05
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-05-13 23:51:29 -0400

    Update Bloom-560M model to support returning new text

--- mlmodelscope/pytorch_agent/models/default/text_to_text/bloom_560m/model.py
+++ mlmodelscope/pytorch_agent/models/default/text_to_text/bloom_560m/model.py
@@ -5,34 +5,26 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 class PyTorch_Transformers_Bloom_560M(PyTorchAbstractClass):
   def __init__(self, config=None):
     self.config = config if config else {} 
-    self.tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m") 
-    self.model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m") 
+    model_id = "bigscience/bloom-560m" 
+    self.tokenizer = AutoTokenizer.from_pretrained(model_id) 
+    self.model = AutoModelForCausalLM.from_pretrained(model_id) 
 
     self.max_new_tokens = self.config.get('max_new_tokens', 32)  
-    self.return_full_text = self.config.get('return_full_text', True)
-
-    if self.return_full_text: 
-      self.preprocess = self.preprocess_full_text 
-      self.postprocess = self.postprocess_full_text 
+    self.return_new_text = self.config.get('return_new_text', False) 
   
   def preprocess(self, input_texts):
     input_ids = self.tokenizer(input_texts, return_tensors="pt", padding=True).input_ids 
-    self.input_ids = input_ids 
+    if self.return_new_text:
+      self.input_ids = input_ids 
     return input_ids 
   
-  def preprocess_full_text(self, input_texts):
-    return self.tokenizer(input_texts, return_tensors="pt", padding=True).input_ids 
-
   def predict(self, model_input): 
     return self.model.generate(model_input, max_new_tokens=self.max_new_tokens) 
 
   def postprocess(self, model_output):
-    input_text = self.tokenizer.batch_decode(self.input_ids, skip_special_tokens=True) 
     output_text = self.tokenizer.batch_decode(model_output, skip_special_tokens=True) 
-    for i in range(len(input_text)): 
-      output_text[i] = output_text[i][len(input_text[i]):]
-    return output_text 
-  
-  def postprocess_full_text(self, model_output):
-    return self.tokenizer.batch_decode(model_output, skip_special_tokens=True) 
+    if self.return_new_text:
+      input_text = self.tokenizer.batch_decode(self.input_ids, skip_special_tokens=True)
+      return [output[len(input_txt):] for output, input_txt in zip(output_text, input_text)]
+    return output_text
   
