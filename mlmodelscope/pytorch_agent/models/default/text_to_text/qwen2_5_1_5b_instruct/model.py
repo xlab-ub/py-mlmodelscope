51814e9c4a1ceb85a7ac7549aec197ee0a988e7b
Changjae Lee <cjlee7128@users.noreply.github.com>
2024-11-17 04:02:32 +0000

    Update Qwen2.5-1.5B-Instruct model in pytorch-agent

--- mlmodelscope/pytorch_agent/models/default/text_to_text/qwen2_5_1_5b_instruct/model.py
+++ mlmodelscope/pytorch_agent/models/default/text_to_text/qwen2_5_1_5b_instruct/model.py
@@ -12,23 +12,37 @@ class PyTorch_Transformers_Qwen2_5_1_5B_Instruct(PyTorchAbstractClass):
 
     self.max_new_tokens = self.config['max_new_tokens'] if 'max_new_tokens' in self.config else 32 
 
-    # if 'messages' in self.config:
-    #   self.messages = self.config['messages'] 
-    #   self.preprocess = self.preprocess_chat 
+    if 'messages' in self.config:
+      self.messages = self.config['messages'] 
+      self.preprocess = self.preprocess_chat
+      self.postprocess = self.postprocess_chat
   
   def preprocess(self, input_texts):
     return self.tokenizer(input_texts, return_tensors="pt", padding=True)
-  
-  # def preprocess_chat(self, input_texts):
-  #   return self.tokenizer.apply_chat_template(self.messages + [{"role": "user", "content": input_texts[0]}], return_tensors="pt") 
+
+  def preprocess_chat(self, input_texts):
+    formatted_messages = []
+    for message in self.messages:
+      role = message.get('role', 'user')
+      content = message.get('content', '')
+      formatted_messages.append({"role": role, "content": content})
+    formatted_messages.append({"role": "user", "content": input_texts[0]})
+    encoded = self.tokenizer.apply_chat_template(formatted_messages, return_dict=True, return_tensors="pt")
+    self.input_ids_shape = encoded['input_ids'].shape
+    return encoded
 
   def predict(self, model_input): 
-    return self.model.generate(
+    outputs = self.model.generate(
       model_input["input_ids"],
       attention_mask=model_input["attention_mask"],
       max_new_tokens=self.max_new_tokens,
       pad_token_id=self.tokenizer.pad_token_id
     )
+    return outputs
 
   def postprocess(self, model_output):
     return self.tokenizer.batch_decode(model_output, skip_special_tokens=True) 
+  
+  def postprocess_chat(self, model_output):
+    response = model_output[0][self.input_ids_shape[-1]+3:]
+    return [self.tokenizer.decode(response, skip_special_tokens=True)]
