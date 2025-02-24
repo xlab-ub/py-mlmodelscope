23e8de2c55fdde879e0098623b758af7adfafaf4
Changjae Lee <cjlee7128@users.noreply.github.com>
2025-02-24 18:07:59 -0500

    Add CrisperWhisper model into pytorch-agent

---
+++ mlmodelscope/pytorch_agent/models/default/automatic_speech_recognition/crisperwhisper/model.py
@@ -0,0 +1,24 @@
+from ....pytorch_abc import PyTorchAbstractClass 
+
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
+import librosa 
+
+class PyTorch_Transformers_CrisperWhisper(PyTorchAbstractClass):
+  def __init__(self, config=None):
+    self.config = config if config else {} 
+    self.processor = AutoProcessor.from_pretrained("nyrahealth/CrisperWhisper")
+    self.model = AutoModelForSpeechSeq2Seq.from_pretrained("nyrahealth/CrisperWhisper")
+
+    self.sampling_rate = self.config['sampling_rate'] if 'sampling_rate' in self.config else 16_000 
+  
+  def preprocess(self, input_audios):
+    for i in range(len(input_audios)):
+      input_audios[i], _ = librosa.load(input_audios[i], sr=self.sampling_rate) 
+    model_input = self.processor(input_audios, sampling_rate=self.sampling_rate, return_tensors="pt").input_features
+    return model_input 
+  
+  def predict(self, model_input): 
+    return self.model.generate(model_input) 
+
+  def postprocess(self, model_output):
+    return self.processor.batch_decode(model_output, skip_special_tokens=True) 
