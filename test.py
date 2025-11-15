160e0bbeb251ff5b34b5b0daf6c8f1b1a889517b
Shivansh Shalabh <shalabhshivansh@gmail.com>
2025-11-14 23:17:13 -0500

    Dry testing with a model for each supported modality

--- test.py
+++ test.py
@@ -4,63 +4,6 @@ from datetime import datetime
 
 # ! This script installs depencies by itself which could be a security risk !
 
-TASK = "image_classification"
-DATASET_NAME_SRC = '[{"src":"https://cdn.pixabay.com/photo/2025/10/17/09/29/nature-9899712_1280.jpg","inputType":"IMAGE"}]'
-MODELS_TO_TEST = [
-    "ai_image_detector_dev_deploy",
-    "beit_base_patch16_224_pt22k_ft22k",
-    "chart_recognizer",
-    "convnext_base_clip_laion2b_augreg_ft_in12k_in1k",
-    "convnext_femto_d1_in1k",
-    "convnext_large_fb_in22k_ft_in1k",
-    "convnext_tiny_in12k_ft_in1k",
-    "convnextv2_nano_fcmae_ft_in22k_in1k",
-    "convnextv2_tiny_fcmae_ft_in1k",
-    "deit_small_patch16_224_fb_in1k",
-    "deit_tiny_patch16_224_fb_in1k",
-    "edgenext_small_usi_in1k",
-    "efficientnet_b0_imagenet",
-    "efficientnet_b0_ra_in1k",
-    "fairface_age_image_detection",
-    "gender_classification",
-    "gender_classification_2",
-    "inception_v3_tv_in1k",
-    "mit_b2",
-    "mit_b3",
-    "mobilenetv3_large_100_ra_in1k",
-    "mobilenetv3_small_100_lamb_in1k",
-    "mobilevit_small",
-    "nsfw_image_detection",
-    "nsfw_image_detector",
-    "pokemon_classifier_gen9_1025",
-    "regnety_032_ra_in1k",
-    "resnet18_a1_in1k",
-    "resnet34_a1_in1k",
-    "resnet50_a1_in1k",
-    "resnet50_fb_swsl_ig1b_ft_in1k",
-    "resnet50_ram_in1k",
-    "resnet_18",
-    "resnet_50",
-    "rexnet_150_nav_in1k",
-    "rorshark_vit_base",
-    "swinv2_tiny_patch4_window16_256",
-    "test.p",
-    "test",
-    "vgg19_tv_in1k",
-    "vit_age_classifier",
-    "vit_base_nsfw_detector",
-    "vit_base_patch16_224",
-    "vit_base_patch16_224_augreg2_in21k_ft_in1k",
-    "vit_base_patch16_224_augreg_in21k",
-    "vit_base_patch16_384",
-    "vit_base_patch32_384_augreg_in21k_ft_in1k",
-    "vit_face_expression",
-    "vit_hybrid_base_bit_384",
-    "vit_small_patch16_224_augreg_in21k_ft_in1k",
-    "vit_tiny_patch16_224_augreg_in21k_ft_in1k",
-    "wide_resnet50_2_racm_in1k",
-]
-
 
 def install_packages_in_conda(package_names):
     """
@@ -387,4 +330,30 @@ def main(task, dataset_name_src, models_to_test):
 
 
 if __name__ == "__main__":
-    main(task=TASK, dataset_name_src=DATASET_NAME_SRC, models_to_test=MODELS_TO_TEST)
+    dirs = [
+        "mlmodelscope/pytorch_agent/models/default/automatic_speech_recognition/wav2vec2_large_xlsr_punjabi/model.py",
+        "mlmodelscope/pytorch_agent/models/default/depth_estimation/coreml_sam2_1_small/model.py",
+        "mlmodelscope/pytorch_agent/models/default/document_visual_question_answering/layoutlm_document_qa/model.py",
+        "mlmodelscope/pytorch_agent/models/default/image_captioning/pix2struct_tiny_random/model.py",
+        "mlmodelscope/pytorch_agent/models/default/image_editing/yoso_normal_v1_8_1/model.py",
+        "mlmodelscope/pytorch_agent/models/default/image_object_detection/dfine_small_coco/model.py",
+        "mlmodelscope/pytorch_agent/models/default/image_semantic_segmentation/seg_zero_7b/model.py",
+        "mlmodelscope/pytorch_agent/models/default/music_generation/qwen2_5_omni_wo_video_1016/model.py",
+        "mlmodelscope/pytorch_agent/models/default/sentiment_analysis/deberta_v3_xsmall_mnli_fever_anli_ling_binary/model.py",
+        "mlmodelscope/pytorch_agent/models/default/text_to_image/stablematerials/model.py",
+        "mlmodelscope/pytorch_agent/models/default/text_to_text/t5_small_booksum/model.py",
+        "mlmodelscope/pytorch_agent/models/default/video_classification/xclip_base_patch16/model.py",
+        "mlmodelscope/pytorch_agent/models/default/visual_question_answering/vl_rethinker_72b/model.py",
+        "mlmodelscope/pytorch_agent/models/default/text_to_image/testllm/model.py",
+        "mlmodelscope/pytorch_agent/models/default/image_classification/siglip2_base_patch16_224/model.py",
+        "mlmodelscope/pytorch_agent/models/default/text_to_image/testllm/model.py",
+        "mlmodelscope/pytorch_agent/models/default/music_generation/speecht5_finetuned_voxpopuli_es/model.py",
+    ]
+    myJson = dict(json.loads(open("./test.json").read()))
+    for modality in myJson:
+        dir = myJson[modality]["dir"]
+        test = myJson[modality]["test"]
+        modelName = [x.split("/")[-2] for x in dirs if dir in x][0]
+        task = [x.split("/")[-3] for x in dirs if dir in x][0]
+        print(task,modelName)
+        run_model_test(modelName,test,"./test_run/", task )
